cc93d7b75c739393e1875d3743667c81b9971dcd
Dusan Jovanov <jovanovdusan1@gmail.com>
2025-03-21 07:19:55 +0100

    fix type

--- src/func.ts
+++ src/func.ts
@@ -135,4 +135,4 @@ export type FuncParamsByName<Params extends readonly Param[]> =
     ? Rest extends readonly Param[]
       ? Record<First["name"], First> & FuncParamsByName<Rest>
       : Record<First["name"], First>
-    : [];
+    : {};
